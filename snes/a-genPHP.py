daf850c9695a013d508566dc01001b1348323624
vuducanh <vuducanhhp200799@gmail.com>
2024-05-25 17:23:15 +0700

    .

---
+++ snes/a-genPHP.py
@@ -0,0 +1,20 @@
+import os
+
+def format_filename(filename):
+    name = filename.replace("_", " ").title()  
+    name = os.path.splitext(name)[0]           
+    return name
+
+def create_file_list():
+    file_list = []
+    for filename in os.listdir():
+        if filename.endswith(".ss"):
+            id = os.path.splitext(filename)[0]
+            name = format_filename(filename)
+            file_list.append({'id': id, 'name': name})
+    return file_list
+
+if __name__ == "__main__":
+    result = create_file_list()
+    for item in result:
+        print(f"array('id' => '{item['id']}', 'name' => '{item['name']}'),")
